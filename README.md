86299b7bcb0013b7804510e27a8286c21f8ad9b3
egidehirwa <egidehirwam@gmail.com>
2023-04-23 00:00:02 +0200

    Scrollytelling graphs - initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Scrollytelling
+
+Hackathon training
+My goal as the developer of my group is to visualize data through graphics.
+
+Here are some sample graphics for training.
---
+++ src/components/Barplot.svelte
@@ -0,0 +1,50 @@
+<script>
+    let bridgeTraffic = [
+{name: "Henry Hudson Bridge", traffic: 58809 },
+{name: "Hugh L. Carey Tunnel", traffic: 54007},
+{name: "Queens-Midtown Tunnel", traffic: 86081 },
+{name: "R.F.K. Memorial Bridge Manhattan Plaza", traffic: 91509 },
+{name: "George Washington Bridge", traffic: 280784 },
+{name: "Holland Tunnel", traffic: 87232 },
+{name: "Lincoln Tunnel", traffic: 109796 },
+    ];
+
+    bridgeTraffic.sort((a, b) => b.traffic - a.traffic);
+</script>
+
+<section>
+    <h1>Bargraph of Bridge traffic</h1>
+<svg>
+{#each bridgeTraffic as bridge, i}
+    <g transform={`translate(300 ${i * 20 + 10})`}>
+        <rect width={bridge.traffic / 1000} height="10" fill="red" />
+        <text font-size="12" text-anchor="end" dominant-baseline="middle" x="-5" y="5">{bridge.name}</text>
+        <text fill="#aaa" font-size="12" dominant-baseline="middle" y="5" x="{bridge.traffic / 1000 + 10}">{bridge.traffic.toLocaleString()}</text>
+    </g>
+{/each}
+</svg>
+</section>
+
+
+<style>
+    p {
+        color: black;
+    }
+
+    svg {
+        border: 1px solid #ddd;
+        width: 100%;
+        height: 500px;
+        
+    }
+
+    section {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+        border: 1px solid red;
+        color: black;
+        
+    }
+</style>
